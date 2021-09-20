5439cd8467026f49800517042d1a7c96a4e2c723
Sasha <sashaveresk3102@gmail.com>
2021-09-20 11:22:43 +0300

    header html was done

---
+++ my-app/src/components/home/home.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import  './home.scss'
+
+
+interface Props {
+
+}
+
+export const Home = (props: Props) => {
+    return (
+        <div className = 'home'>
+            Home
+        </div>
+    )
+}
---
+++ my-app/src/components/monthStatistic/monthStatistic.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import'./monthStatistic.scss'
+interface Props {
+
+}
+
+export const MonthStatistic = (props: Props) => {
+    return (
+        <div className = ' MonthStatistic'>
+            Month Statistic
+        </div>
+    )
+}
